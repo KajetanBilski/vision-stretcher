718e5b3685af70846b7dcfe8ad37b002482241a3
ProtossLuigi <protoss0@wp.pl>
2022-12-05 16:35:57 +0100

    added second metric (to be tested)

--- metrics/foreground_metric2.py
+++ metrics/foreground_metric2.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from sklearn.metrics import jaccard_score
 
 def crop_image(img):
     minx, miny, maxx, maxy = 0, 0, img.shape[1], img.shape[0]
@@ -16,10 +17,17 @@ def crop_image(img):
 def foreground_score(src_img, rec_img, predictor, coi):
     src_outs, rec_outs = predictor(src_img), predictor(rec_img)
     src_masks, rec_masks = src_outs['instances'].pred_masks.cpu().numpy(), rec_outs['instances'].pred_masks.cpu().numpy()
+    filter_func = lambda x: x[i].item() in coi
+    src_coi, rec_coi = list(map(filter_func, src_outs['instances'].pred_classes)), list(map(filter_func, rec_outs['instances'].pred_classes))
+    src_masks, rec_masks = src_masks[src_coi], rec_masks[rec_coi]
     if len(src_masks) != len(rec_masks):
-        return 0.
+        return np.nan
+    values, weights = [], []
     for i in len(src_masks):
         src_mask, rec_mask = crop_image(src_masks[i]), crop_image(rec_masks[i])
         rec_mask = cv2.resize(rec_mask.astype(np.uint8) * 255, src_mask.shape[::-1])
-        _, rec_mask = cv2.threshold(rec_mask, None, 255, cv2.THRESH_OTSU)
-        
+        _, rec_mask = cv2.threshold(rec_mask, 127, 255, cv2.THRESH_BINARY)
+        weights.append(np.sum(src_mask))
+        values.append(jaccard_score(src_mask, rec_mask, average="micro"))
+    return np.average(values, weights)
+    
